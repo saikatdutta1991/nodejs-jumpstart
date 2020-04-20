37c437983d478dc23418ea4aa0d70598eb0d483a
Saikat Dutta <saikatdutta@Saikats-MacBook-Air.local>
2020-04-20 19:14:38 +0530

    socket io added

--- src/main/index.js
+++ src/main/index.js
@@ -1,51 +1,10 @@
-const express = require("express");
-require("express-async-errors"); // handle async route errors
-const errorHandler = require("./app/commons/errorHandler");
-const bodyParser = require("body-parser");
-const logger = require("./app/commons/logger");
-const Router = require("./boot/router");
-const Cron = require("./boot/cron");
-const GlobalVars = require("./boot/globalVars");
+const http = require("http");
+const app = require("./app");
+const io = require("./app/commons/socket-io");
+
 const port = process.env.PORT;
 
-/** creating app instance */
-const app = express();
-
-/** cross domain api call */
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-
-  next();
-});
-/** cross domain api call end*/
-
-/** setup body parsers */
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-/** setup body parsers end*/
-
-/** booting app start */
-GlobalVars.load();
-const router = new Router(app); // this router is not express router
-router.load();
-const cron = new Cron();
-cron.register();
-/** booting app end */
-
-app.use(errorHandler); // handle error and send specific request
-
-app.listen(port, () =>
-  logger.info(`Server started & listening on port ${port}`)
-);
-
-module.exports = app;
+/** Create HTTP server */
+const server = http.createServer(app);
+io.attach(server);
+server.listen(port);
